8414415dcd93539223a4527673b7d6319b2e81b6
martinsamb <martinsamb@gmail.com>
2019-10-06 17:37:26 -0300

    PT2

---
+++ TP_2/ArrayEmployees.h
@@ -0,0 +1,36 @@
+#ifndef ARRAYEMPLOYEES_H_INCLUDED
+#define ARRAYEMPLOYEES_H_INCLUDED
+
+#define TEXT_SIZE 51
+
+typedef struct
+{
+    int idUnico;
+    int isEmpty;
+    //-----------------
+    char name[TEXT_SIZE];
+    int sector;
+    float salary;
+    char lastName[TEXT_SIZE];
+
+}Employee;
+
+
+
+#endif // ARRAYEMPLOYEES_H_INCLUDED
+
+int employee_Inicializar(Employee array[], int size);
+int employee_buscarEmpty(Employee array[], int size, int* posicion);
+int employee_buscarID(Employee array[], int size, int valorBuscado, int* posicion);
+int employee_alta(Employee array[], int size, int* contadorID);
+int employee_baja(Employee array[], int sizeArray);
+int employee_modificar(Employee array[], int sizeArray);
+int employee_ordenar(Employee array[],int size);
+int employee_listar(Employee array[], int size);
+int employee_informarSalario(Employee array[], int size);
+void menuPrincipal();
+void menuInformes();
+
+
+
+
